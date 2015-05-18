e9ca89838365e1d1f62274c25f0353f3d1c54b90
jonyguan <jonyguan@126.com>
2015-05-18 10:11:42 +0800

    修改数据连接

--- api/date/config.php
+++ api/date/config.php
@@ -7,11 +7,5 @@ $cfg_dbpwd = '123456';
 $cfg_dbprefix = 'wp_';
 $cfg_db_language = 'utf8';
 
-// $cfg_dbhost = 'qdm11447523.my3w.com';
-// $cfg_dbname = 'qdm11447523_db';
-// $cfg_dbuser = 'qdm11447523';
-// $cfg_dbpwd = '******';
-// $cfg_dbprefix = 'wp_';
-// $cfg_db_language = 'utf8';
 
 ?>
